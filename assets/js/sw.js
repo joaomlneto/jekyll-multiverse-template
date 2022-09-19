7f390b5172845ea28f5cf9040cfe5a32fce4bfec
razaqfatiu <razaqfatiu@gmail.com>
2022-09-19 08:49:24 +0100

    chore: made template pwa

---
+++ assets/js/sw.js
@@ -0,0 +1,41 @@
+const cacheName = 'jekyll-v1';
+const staticAssets = ['/assets/', '/_layouts'];
+
+self.addEventListener('install', async (e) => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', async (e) => {
+  const req = e.request;
+  const url = new URL(req.url);
+
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
+  }
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
+}
